574d4b1167d1c983f31a46808ccf2e577fe3e993
gabri-viane <gabriviane@hotmail.it>
2024-04-27 23:55:16 +0200

    Initial commit

---
+++ src/main/java/net/vnleng/programmablegui/ProgrammableGui.java
@@ -0,0 +1,27 @@
+package net.vnleng.programmablegui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Plugin class for the ProgrammableGUI api
+ */
+public final class ProgrammableGui extends JavaPlugin {
+
+    /**
+     * Empty constructor
+     */
+    public ProgrammableGui(){
+
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
